b6c81fa84ff5e5bd622a4b67ed18af3fa1f2aa8e
weilyuwang <weilyuwang@gmail.com>
2020-05-18 14:09:16 -0400

    add next button

--- lib/screens/onboarding/components/onboard_page.dart
+++ lib/screens/onboarding/components/onboard_page.dart
@@ -11,66 +11,97 @@ class OnboardPage extends StatefulWidget {
 }
 
 class _OnboardPageState extends State<OnboardPage> {
+  _nextButtonPressed() {
+    print('next button pressed!');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: widget.pageModel.primeColor,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(top: 32.0),
-            child: Image.asset(widget.pageModel.imagePath),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32.0),
-            child: Container(
-              height: 250.0,
-              width: double.infinity,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: Text(
-                      widget.pageModel.caption,
-                      style: TextStyle(
-                        fontSize: 24.0,
-                        letterSpacing: 1,
-                        color: widget.pageModel.accentColor.withOpacity(0.8),
-                        fontStyle: FontStyle.normal,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: Text(
-                      widget.pageModel.subhead,
-                      style: TextStyle(
-                        fontSize: 40.0,
-                        fontWeight: FontWeight.bold,
-                        color: widget.pageModel.accentColor,
-                        letterSpacing: 1,
-                        fontStyle: FontStyle.normal,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: Text(
-                      widget.pageModel.description,
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        color: widget.pageModel.accentColor.withOpacity(0.9),
-                      ),
-                    ),
-                  ),
-                ],
+    return Stack(
+      children: <Widget>[
+        Container(
+          color: widget.pageModel.primeColor,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 32.0),
+                child: Image.asset(widget.pageModel.imagePath),
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                child: Container(
+                  height: 250.0,
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Text(
+                          widget.pageModel.caption,
+                          style: TextStyle(
+                            fontSize: 24.0,
+                            letterSpacing: 1,
+                            color:
+                                widget.pageModel.accentColor.withOpacity(0.8),
+                            fontStyle: FontStyle.normal,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: Text(
+                          widget.pageModel.subhead,
+                          style: TextStyle(
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.bold,
+                            color: widget.pageModel.accentColor,
+                            letterSpacing: 1,
+                            fontStyle: FontStyle.normal,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Text(
+                          widget.pageModel.description,
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color:
+                                widget.pageModel.accentColor.withOpacity(0.9),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+        Align(
+          alignment: Alignment.centerRight,
+          child: Container(
+            width: 50.0,
+            height: double.infinity,
+            color: Colors.green,
+            child: Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 24.0),
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.arrow_back,
+                      color: widget.pageModel.primeColor,
+                      size: 32.0,
+                    ),
+                    onPressed: _nextButtonPressed,
+                  ),
+                )),
+          ),
+        ),
+      ],
     );
   }
 }
